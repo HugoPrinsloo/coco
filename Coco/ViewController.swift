318bf06bf4d8a6ac7b31817b290f3bcb9e05eff2
Hugo Prinsloo <hugo.prinsloo@icloud.com>
2018-03-31 13:48:27 +0200

    Added firebase

--- Coco/ViewController.swift
+++ Coco/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import Firebase
+
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var db: DatabaseReference?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        db = Database.database().reference().child("Test").child("currentState")
+        db?.setValue("Let's Start")
+        db?.observe(DataEventType.value, with: { (snap) in
+            self.label.text = "\(String(describing: snap.value!))"
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cocoButtonTapped(_ sender: UIButton) {
+        db?.setValue("Coco")
     }
-
-
+    
+    @IBAction func awesomeButtonTapped(_ sender: UIButton) {
+        db?.setValue("is Awesome")
+    }
 }
 
